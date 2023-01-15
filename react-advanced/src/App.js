7691a7544dee1c9cdaa7f1cd96f6d72eea5019c1
Umut-Polat-GM <umut.polat.272@gmail.com>
2023-01-15 23:51:10 +0300

    memo-useMemo part finished 10 | React.js

--- react-advanced/src/App.js
+++ react-advanced/src/App.js
@@ -14,7 +14,7 @@ import React from 'react'
 // import Setup from './tutorial/8-useContext/setup/1-context-api'
 // import Setup from './tutorial/9-custom-hooks/setup/1-fetch-example'
 // import Setup from './tutorial/10-prop-types/setup/index'
-import Setup from './tutorial/11-react-router/final/Home'
+import Setup from './tutorial/12-memo-useMemo-useCallback/setup'
 function App() {
   return (
     <div className='container'>
