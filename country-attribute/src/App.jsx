074ee728baa7d27f2522db9e84c9dbc28efdb792
Umut-Polat-GM <umut.polat.272@gmail.com>
2023-03-24 08:08:35 +0300

    axios and API use

--- country-attribute/src/App.jsx
+++ country-attribute/src/App.jsx
@@ -1,12 +1,28 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [countries, setCountries] = useState([]);
 
+  useEffect(()=>{
+    axios.get("https://restcountries.com/v3.1/all")
+      .then(response => setCountries(response.data))
+      .catch(error => console.log({error}))
+  }, []);
+ console.log(countries)
   return (
     <div>
-      
+      {countries?.map((country, index) => {
+        return (
+          <div key={index}>
+              <h2>{country.name.common}</h2>
+              <h4>{country.capital}</h4>
+              <p>
+                <img src={country.flags.png} alt={country.flags.alt}/>
+              </p>
+          </div>
+        )
+      })}
     </div>
   )
 }
