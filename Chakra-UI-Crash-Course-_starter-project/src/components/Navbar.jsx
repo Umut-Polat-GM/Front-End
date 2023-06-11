49e0e968d1d3bed362d661b4202353d4b2b0440e
Umut Polat <62594144+Umut-Polat-GM@users.noreply.github.com>
2023-06-11 23:49:06 +0300

    chore: 1. lesson finish | Chakra UI

---
+++ Chakra-UI-Crash-Course-_starter-project/src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import {
+  Box,
+  Button,
+  Flex,
+  HStack,
+  Heading,
+  Spacer,
+  Text,
+} from "@chakra-ui/react";
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <Flex bg={"grey"} p={".4rem"} alignItems={"center"}>
+        <Heading color={"orange.400"}>Umut Task</Heading>
+        <Spacer />
+        <HStack spacing={"1.4rem"}>
+          <Box bg={"gray.200"} p={".6rem"} borderRadius={"md"} fontWeight={"bold"}>M</Box>
+          <Text>umut@bloblocom</Text>
+          <Button colorScheme="purple">Logout</Button>
+        </HStack>
+      </Flex>
+
+      {/* <Flex bg={'grey'} justify={`space-between`}>
+        <Box w={`100px`} h={`100px`} bg={`yellow.200`}>1</Box>
+        <Box w={`100px`} h={`100px`} bg={`blue.200`}>2</Box>
+        <Box w={`100px`} h={`100px`} bg={`orange.200`} flexGrow={3}>3</Box>
+        <Box w={`100px`} h={`100px`} bg={`red.200`} flexGrow={2}>4</Box>
+        <Box w={`100px`} h={`100px`} bg={`purple.200`} flexGrow={1}>5</Box>
+      </Flex> */}
+    </div>
+  );
+}
+
+export default Navbar;
--- Chakra-UI-Crash-Course-_starter-project/src/pages/Dashboard.jsx
+++ Chakra-UI-Crash-Course-_starter-project/src/pages/Dashboard.jsx
@@ -1,5 +1,33 @@
+import { Container, Heading, Text, Box } from "@chakra-ui/react";
+
 export default function Dashboard() {
+
+  const boxStyle = {
+    p:".5rem",
+    color:"red",
+    bg:"blue.300",
+    textAlign:"center",
+    borderRadius:"md",
+    filter:"drop-shadow(0 0 0.5rem blue)",
+    transeition:"all .5s ease",
+    _hover:{
+      bg:"yellow.400",
+      color:"black",
+    }
+  }
+
   return (
-    <div>Dashboard</div>
+    <Container as={"section"} maxWidth={"4xl"} py={"1rem"}>
+      <Heading my="2rem" p=".5rem">Chakra UI Components</Heading>
+      <Text marginLeft="1rem">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, voluptatem.</Text>
+      <Text ml="1rem" color={"blue.300"} fontWeight={"bold"}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, voluptatem.</Text>
+
+      <Box my={"2rem"} bg={"orange.300"} p={"1rem"}>
+        <Text color={"black"}>Lorem ipsum dolor sit amet.</Text>
+      </Box>
+      <Box sx={boxStyle}>
+        <Text> Hello Umut</Text>
+      </Box>
+    </Container>
   )
 }
