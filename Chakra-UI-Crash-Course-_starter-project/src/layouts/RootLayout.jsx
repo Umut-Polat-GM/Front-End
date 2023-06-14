ed895d6fd93d5a47dcc272b7a8754404c41163b0
Umut Polat <62594144+Umut-Polat-GM@users.noreply.github.com>
2023-06-15 01:13:22 +0300

    feat: card components finish

--- Chakra-UI-Crash-Course-_starter-project/src/layouts/RootLayout.jsx
+++ Chakra-UI-Crash-Course-_starter-project/src/layouts/RootLayout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import { Grid, GridItem } from "@chakra-ui/react";
+import Sidebar from "../components/Sidebar";
 
 export default function RootLayout() {
   return (
@@ -9,10 +10,10 @@ export default function RootLayout() {
         as="aside"
         colSpan={{ base: 6, lg: 2, xl: 1 }}
         bg="purple.400"
-        minHeight="100vh"
+        minHeight={{base: "100vh"}}
         p={{ base: "1rem", lg: "2rem" }}
       >
-        <span>sidebar</span>
+        <Sidebar />
       </GridItem>
       <GridItem as="main" colSpan={{ base: 6, lg: 4, xl: 5 }} p="40px">
         <Navbar />
--- Chakra-UI-Crash-Course-_starter-project/src/pages/Dashboard.jsx
+++ Chakra-UI-Crash-Course-_starter-project/src/pages/Dashboard.jsx
@@ -1,27 +1,73 @@
-import { SimpleGrid, Box } from '@chakra-ui/react'
-import React from 'react'
+import { ViewIcon, EditIcon } from "@chakra-ui/icons";
+import {
+  SimpleGrid,
+  Box,
+  Text,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Heading,
+  Flex,
+  HStack,
+  Button,
+  Divider,
+} from "@chakra-ui/react";
+import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 function Dashboard() {
+  const tasks = useLoaderData();
+
   return (
-    <SimpleGrid p="10px" spacing={10} minChildWidth={"250px"}>
-      <Box bg={"white"} w={"200px"} h="200px" border="1px solid">
-        <Text>Hello</Text>
-      </Box>
-      <Box bg={"white"} w={"200px"} h="200px" border="1px solid"></Box>
-      <Box bg={"white"} w={"200px"} h="200px" border="1px solid"></Box>
-      <Box bg={"white"} w={"200px"} h="200px" border="1px solid"></Box>
+    <SimpleGrid p="10px" spacing={10} minChildWidth={"300px"}>
+      {tasks &&
+        tasks.map((task) => (
+          <Card
+            key={task.id}
+            borderTop={".5rem"}
+            borderColor={"purple.400"}
+            bg={"white"}
+          >
+            <CardHeader>
+              <Flex gap={3}>
+                <Box w="50px" h={"50px"}>
+                  <Text>AV</Text>
+                </Box>
+                <Box>
+                  <Heading as="h3" size={"dm"}>
+                    {task.title}
+                  </Heading>
+                  <Text>by {task.author}</Text>
+                </Box>
+              </Flex>
+            </CardHeader>
+            <CardBody color="gray.500">
+              <Text>{task.description}</Text>
+            </CardBody>
 
-      <Box bg={"white"} w={"200px"} h="200px" border="1px solid"></Box>
-      <Box bg={"white"} w={"200px"} h="200px" border="1px solid"></Box>
-      <Box bg={"white"} w={"200px"} h="200px" border="1px solid"></Box>
-      <Box bg={"white"} w={"200px"} h="200px" border="1px solid"></Box>
+            <Divider borderColor="gray.200" />
 
-      <Box bg={"white"} w={"200px"} h="200px" border="1px solid"></Box>
-      <Box bg={"white"} w={"200px"} h="200px" border="1px solid"></Box>
-      <Box bg={"white"} w={"200px"} h="200px" border="1px solid"></Box>
-      <Box bg={"white"} w={"200px"} h="200px" border="1px solid"></Box>
+            <CardFooter>
+              <HStack>
+                <Button variant="ghost" leftIcon={<ViewIcon />}>
+                  Watch
+                </Button>
+                <Button variant="ghost" leftIcon={<EditIcon />}>
+                  Comment
+                </Button>
+              </HStack>
+            </CardFooter>
+          </Card>
+        ))}
     </SimpleGrid>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
+
+export const taskLoader = async () => {
+  const res = await fetch("http://localhost:3000/tasks");
+
+  return res.json();
+};
