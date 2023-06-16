0cff7cd9cfc988e2d7329070edfdba6d4b2fa4d9
Umut Polat <62594144+Umut-Polat-GM@users.noreply.github.com>
2023-06-16 23:59:11 +0300

    chore: profile and create page finish

--- Chakra-UI-Crash-Course-_starter-project/src/pages/Create.jsx
+++ Chakra-UI-Crash-Course-_starter-project/src/pages/Create.jsx
@@ -1,5 +1,45 @@
+import { Box, FormControl, FormLabel, Input, FormHelperText, Textarea, Checkbox, Button } from "@chakra-ui/react";
+import { Form, redirect } from "react-router-dom";
+
 export default function Create() {
   return (
-    <div>Create</div>
-  )
+    <Box maxW={"480px"}>
+      <Form method="post" action="/create">
+        <FormControl isRequired mb="40px">
+          <FormLabel>Task name:</FormLabel>
+          <Input type="text" name="title" />
+          <FormHelperText>Enter a description task name.</FormHelperText>
+        </FormControl>
+
+        <FormControl isRequired mb="40px">
+          <FormLabel>Task description:</FormLabel>
+          <Textarea
+            placeholder="Enter a detailed description for the task..."
+            name="description"
+          />
+        </FormControl>
+
+        <FormControl display={"flex"} alignItems={"center"} mb={"40px"}>
+          <Checkbox name="isPriorty"  size={"lg"} colorScheme="purple"/>
+          <FormLabel mb={"0"} ml={"10px"}>Is this task a priority?</FormLabel>
+        </FormControl>
+        <Button type="submit" >Submit</Button>
+      </Form>
+    </Box>
+  );
+}
+
+
+export const createAction = async ({ request }) => ({
+  const data = await request.formData()
+
+  const task = {
+    title: data.get('title'),
+    description: data.get('description'),
+    isPriority: data.get('isPriority') === ''
+  }
+
+  console.log(data);
+
+  return redirect('/')
 }
--- Chakra-UI-Crash-Course-_starter-project/src/pages/Profile.jsx
+++ Chakra-UI-Crash-Course-_starter-project/src/pages/Profile.jsx
@@ -1,5 +1,57 @@
+import { EmailIcon, ChatIcon, StarIcon, CheckCircleIcon, WarningIcon } from "@chakra-ui/icons";
+import {
+  Tabs,
+  TabList,
+  TabPanels,
+  TabPanel,
+  Tab,
+  ListItem,
+  List,
+  ListIcon,
+} from "@chakra-ui/react";
+import React from "react";
+
 export default function Profile() {
   return (
-    <div>Profile</div>
-  )
+    <Tabs mt={"40px"} p={"20px"} colorScheme="purple" variant={"enclosed"}>
+      <TabList>
+        <Tab _selected={{ color: "white", bg: "purple.400" }}>Account Info</Tab>
+        <Tab _selected={{ color: "white", bg: "purple.400" }}>Task History</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel>
+          <List spacing={4}>
+            <ListItem>
+              <ListIcon as={EmailIcon} />
+              Email: mario@netninja.dev
+            </ListItem>
+            <ListItem>
+              <ListIcon as={ChatIcon} />
+              Lorem ipsum dolor sit amet.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={StarIcon} />
+              Lorem ipsum dolor sit amet.
+            </ListItem>
+          </List>
+        </TabPanel>
+        <TabPanel>
+          <List spacing={4}>
+            <ListItem>
+              <ListIcon as={CheckCircleIcon} color={"teal.400"}/>
+              Email: mario@netninja.dev
+            </ListItem>
+            <ListItem>
+              <ListIcon as={WarningIcon} color={"red.400"}/>
+              Lorem ipsum dolor sit amet.
+            </ListItem>
+            <ListItem>
+              <ListIcon as={CheckCircleIcon}  color={"green.400"}/>
+              Lorem ipsum dolor sit amet.
+            </ListItem>
+          </List>
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
+  );
 }
